43c08aff60d67abbdf314dd0c1941a69b1e59339
evg <noemail>
2024-06-03 17:42:25 +0700

    MeshUtils and DefaultParams moved to src folder

---
+++ src/DefaultParams.py
@@ -0,0 +1,34 @@
+DEFAULT_PARAMS = {
+    "remesh_p": {"mode": "BLOCKS", "octree_depth": 8}, 
+
+    "lst_deforms": [
+        {"deform_method": "BEND", "deform_axis": "X", "angle": "280"},
+        {"deform_method": "BEND", "deform_axis": "Y", "angle": "280"}, 
+        {"deform_method": "BEND", "deform_axis": "Z", "angle": "280"}, 
+        {"deform_method": "BEND", "deform_axis": "X", "angle": "360"},
+        {"deform_method": "BEND", "deform_axis": "Y", "angle": "360"},
+        {"deform_method": "BEND", "deform_axis": "Z", "angle": "360"},
+        {"deform_method": "BEND", "deform_axis": "X", "angle": "180"},
+        {"deform_method": "BEND", "deform_axis": "Y", "angle": "180"},
+        {"deform_method": "BEND", "deform_axis": "Z", "angle": "180"},
+        {"deform_method": "BEND", "deform_axis": "X", "angle": "90"},
+        {"deform_method": "BEND", "deform_axis": "Y", "angle": "90"},
+        {"deform_method": "BEND", "deform_axis": "Z", "angle": "90"},
+        {"deform_method": "BEND", "deform_axis": "X", "angle": "45"},
+        {"deform_method": "BEND", "deform_axis": "Y", "angle": "45"},
+        {"deform_method": "BEND", "deform_axis": "Z", "angle": "45"},
+        {"deform_method": "BEND", "deform_axis": "X", "angle": "60"},
+        {"deform_method": "BEND", "deform_axis": "Y", "angle": "60"},
+        {"deform_method": "BEND", "deform_axis": "Z", "angle": "60"},
+        {"deform_method": "BEND", "deform_axis": "X", "angle": "560"},
+        {"deform_method": "BEND", "deform_axis": "Y", "angle": "560"},
+        {"deform_method": "BEND", "deform_axis": "Z", "angle": "560"},
+        {"deform_method": "STRETCH", "deform_axis": "X", "factor": 1.163},
+        {"deform_method": "STRETCH", "deform_axis": "Y", "factor": 1.163},
+        {"deform_method": "STRETCH", "deform_axis": "Z", "factor": 1.163},
+        {"deform_method": "TAPER", "deform_axis": "Y", "factor": -1.537}
+    ],
+
+    "need_remesh" : True,
+    "apply_modifiers" : False
+}
